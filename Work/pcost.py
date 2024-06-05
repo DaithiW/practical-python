0b852369ee1342b0d251bb2f4b9cffec331a9ad7
DaitiW <davidofwalsh@gmail.com>
2024-06-05 20:31:44 +0100

    section 2.2 complete

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,30 +1,39 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
+
 def portfolio_cost(filename):
     "calculates portfolio cost from a csv"
     f = open(filename, "rt")
+    rows = csv.reader(f)
+    headers = next(rows)
 
-    headers = next(f).split(",")
     shares = []
     price = []
 
-    for line in f:
+    for row in rows:
         try:
-            shares.append(line.split(",")[1])
-            price.append(line.split(",")[2])
+            shares.append(row[1])
+            price.append(row[2])
         except ValueError:
             print("Could not parse file on line", line)
-
+    print(f"shares {shares}")
+    print(f"price {price}")
     value = 0
     for i in range(len(shares)):
         try:
-            value += float(shares[i]) * float(price[i])
+            value += int(shares[i]) * float(price[i])
         except ValueError:
-            print("Could not parse file on line", line)
+            print("Could not parse file on line", i)
 
     return value
 
-
-cost = portfolio_cost("Data/missing.csv")
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = "Data/portfolio.csv"
+    
+cost = portfolio_cost(filename)
 print("Total cost ", cost)
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,66 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+
+def read_portfolio(filename):
+    "this is a some info"
+    # assuming this is for prices when purchased...
+    portfolio = []
+    with open(filename, "rt") as f:
+        rows = csv.reader(f)
+
+        # each stock should be a dict, not tuple - keys - "name" "shares" "price"
+        headers = next(rows)
+        for row in rows:
+            try:
+                holding = {
+                    "name": row[0],
+                    "shares": int(row[1]),
+                    "price": float(row[2]),
+                }
+                # return the portfolio list
+                portfolio.append(holding)
+            except ValueError:
+                print("unable to parse file on line", row)
+
+        return portfolio
+
+
+def read_prices(filename):
+    "some more info"
+    # assume this is for current price
+    
+    price_dict = {}
+    with open(filename, "rt") as f:
+        rows = csv.reader(f)
+
+        # assume headers
+
+        for row in rows:
+            try:
+                price_dict[row[0]] = float(row[1])
+            except IndexError:
+                print("some error on row", row)
+
+        return price_dict
+
+def calc_diff(portfolio, prices):
+    "calculates sum diff from purchase to current"
+    # for name in i in portfolio...
+    names = []
+
+    net = 0
+    value = 0
+    print(portfolio)
+    for i in range(len(portfolio)):
+        name = portfolio[i]["name"]
+        shares = int(portfolio[i]["shares"])
+        price = float(portfolio[i]["price"])
+        value += prices[name] * shares
+        
+        gain = (prices[name] * shares) - (price * shares)
+        print(f"for {name} the net change is {gain}")
+        net += gain
+    return net, value
